def8ce321453222ea40894a98ee5603684460e6b
Luo Yuan <Rowen.luo@grapecity.com>
2019-12-30 10:14:11 +0800

    update razor pages

--- ContosoUniversity/Pages/Students/Index.cshtml.cs
+++ ContosoUniversity/Pages/Students/Index.cshtml.cs
@@ -24,13 +24,22 @@ namespace ContosoUniversity
         public string CurrentFilter { get; set; }
         public string CurrentSort { get; set; }
 
-        public IList<Student> Students { get; set; }
+        public PaginatedList<Student> Students { get; set; }
 
-        public async Task OnGetAsync(string sortOrder, string searchString)
+        public async Task OnGetAsync(string sortOrder, string searchString, string currentFilter, int? pageIndex)
         {
+            CurrentSort = sortOrder;
             NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
             DateSort = sortOrder == "Date" ? "date_desc" : "Date";
 
+            if (searchString != null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
             CurrentFilter = searchString;
 
             IQueryable<Student> studentsIQ = from s in _context.Students
@@ -56,8 +65,9 @@ namespace ContosoUniversity
                     studentsIQ = studentsIQ.OrderBy(s => s.LastName);
                     break;
             }
-
-            Students = await studentsIQ.AsNoTracking().ToListAsync();
+            int pageSize = 3;
+            Students = await PaginatedList<Student>.CreateAsync(
+                studentsIQ.AsNoTracking(), pageIndex ?? 1, pageSize);
         }
     }
 }
